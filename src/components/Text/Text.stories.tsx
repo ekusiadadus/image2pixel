730ca46c0132fc8c3722c28a4394a8094fd0a6f1
ekusiadadus <ekusiadadus@gmail.com>
2024-02-28 17:10:52 +0900

    chore: updage page

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { StoryObj, Meta } from '@storybook/react'
+import { StoryFn, Meta } from '@storybook/react'
 
-import { Text } from './Text'
+import { Text, TextProps } from './Text'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Text/Text',
   component: Text
 } as Meta<typeof Text>
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: StoryObj<typeof Text> = (args) => <Text {...args} />
+// StoryObjではなく、Storyを使用します。
+const Template: StoryFn<TextProps> = (args) => <Text {...args} />
 
 export const Default = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
   text: 'hello, world'
 }
